82bf4e52007566857059e6b439d1fcfd4dcbf55a
Dullah2k <dullahmwarlabu@gmail.com>
2024-11-07 09:31:56 +0300

    created urls file for the account app

---
+++ account/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from account import views
+
+urlpatterns = [
+  path('', include('django.contrib.auth.urls')),
+  path('', views.dashboard, name='dashboard'),
+]
--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -5,7 +5,4 @@ from account import views
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-  path('login/', auth_views.LoginView.as_view(), name='login'),
-	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-  path('', views.dashboard, name='dashboard'),
 ]
