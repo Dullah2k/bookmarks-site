291e4c97671c7fe78f4e79bf7237589bdc6947ad
Dullah2k <dullahmwarlabu@gmail.com>
2024-11-07 12:04:44 +0300

    created environment to work with OAuth2.0

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vl7@!ij_f8u-%b0dp_dsk0e$#qo_63s_sygt#o679xn+0jy^2u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
