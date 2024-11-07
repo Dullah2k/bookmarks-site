6f4a72d1cf2324af3064479b762fec50929ea025
Dullah2k <dullahmwarlabu@gmail.com>
2024-11-07 10:04:51 +0300

    successfully created a uiser registration view

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 from account import views
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
+  path('account/', include('account.urls')),
 ]
