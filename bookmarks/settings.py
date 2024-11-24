2f7d023b0328a6fc112c20b98f11399e9a2951aa
Dullah2k <dullahmwarlabu@gmail.com>
2024-11-24 10:52:59 +0300

    Creating a profile for users that register with social authentication

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -149,3 +149,16 @@ SOCIAL_AUTH_FACEBOOK_SECRET = '4430126d6071a9466f682e81cad41669'
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '695516002381-9pvacuqr1es35qcb56afrr3l2shnlff1.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
 SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI = 'http://mysite.com:8000/social-auth/complete/google-oauth2/'
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
